5a40925c00daba07fc9c4782f62b39a3f34f7317
Tanya Lyakh <Ttanka1110girl@gmail.com>
2017-03-05 17:16:51 +0100

    full version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-
-	$(document).ready(function(){
+$(document).ready(function(){
 		
-		
-		
-	});
+	var first = $('div.grid.grid-12');
+	first.css('background-color','red');
+	
+	var second = $('.nav > a[href^=http]');
+	second.css('color','green');
+	
+	var third = $('input[type="radio"]:not(:checked), input[type="checkbox"]:not(:checked)');
+	
+	var forth = $('div#text p').not(":has(*)").first();
+	forth.css('border','1px solid red');
+	
+	var fift = $(".pigination-item").not('span');
+	
+});
